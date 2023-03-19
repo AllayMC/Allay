c872fcdc3fdec9ecc40be9cc0e94d1863130a676
刀哥 <3523206925@qq.com>
2023-03-19 16:52:28 +0800

    IntProperty min & max

--- Allay-API/src/main/java/cn/allay/block/component/property/IntProperty.java
+++ Allay-API/src/main/java/cn/allay/block/component/property/IntProperty.java
@@ -1,16 +1,25 @@
 package cn.allay.block.component.property;
 
+import lombok.Getter;
+
 /**
  * Author: daoge_cmd <br>
  * Date: 2023/3/19 <br>
  * Allay Project <br>
  */
+@Getter
 public final class IntProperty extends BaseBlockProperty<Integer> {
-    public IntProperty(String name) {
-        super(name);
+
+    private final int min;
+    private final int max;
+
+    public IntProperty(String name, int min, int max) {
+        this(name, 0, min, max);
     }
 
-    public IntProperty(String name, Integer value) {
+    public IntProperty(String name, Integer value, int min, int max) {
         super(name, value);
+        this.min = min;
+        this.max = max;
     }
 }
