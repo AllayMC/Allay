f75488317785b7d747c33d770d5d8f41fb9870df
刀哥 <3523206925@qq.com>
2023-03-26 13:49:21 +0800

    BlockRegistry

---
+++ Allay-API/src/main/java/cn/allay/block/BlockRegistry.java
@@ -0,0 +1,26 @@
+package cn.allay.block;
+
+import cn.allay.block.type.BlockType;
+import cn.allay.identifier.Identifier;
+import cn.allay.registery.SimpleMappedRegistry;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Author: daoge_cmd <br>
+ * Date: 2023/3/26 <br>
+ * Allay Project <br>
+ */
+public final class BlockRegistry extends SimpleMappedRegistry<Identifier, BlockType<?>, Map<Identifier, BlockType<?>>>{
+
+    private static final BlockRegistry INSTANCE = new BlockRegistry();
+
+    private BlockRegistry() {
+        super(null, input -> new HashMap<>());
+    }
+
+    public static BlockRegistry getInstance() {
+        return INSTANCE;
+    }
+}
--- Allay-API/src/main/java/cn/allay/block/type/BlockType.java
+++ Allay-API/src/main/java/cn/allay/block/type/BlockType.java
@@ -2,12 +2,6 @@ package cn.allay.block.type;
 
 import cn.allay.block.Block;
 import cn.allay.block.definition.BlockDefinition;
-import cn.allay.identifier.Identifier;
-import cn.allay.registery.MappedRegistry;
-import cn.allay.registery.SimpleMappedRegistry;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Author: daoge_cmd <br>
@@ -15,8 +9,6 @@ import java.util.Map;
  * Allay Project <br>
  */
 public interface BlockType<T extends Block> {
-
-    MappedRegistry<Identifier, BlockType<?>, Map<Identifier, BlockType<?>>> REGISTRY = SimpleMappedRegistry.create(i -> new HashMap<>());
 
     BlockDefinition<T> getDefinition();
 
