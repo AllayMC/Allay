570c9a67887358d03970beb0361de38e2718ce28
刀哥 <3523206925@qq.com>
2023-07-01 14:05:19 +0800

    feat: Start working on world/chunk logic

---
+++ Allay-API/src/main/java/cn/allay/api/world/chunk/ChunkLoader.java
@@ -0,0 +1,13 @@
+package cn.allay.api.world.chunk;
+
+import cn.allay.api.math.location.FixedLoc;
+
+/**
+ * Allay Project 2023/7/1
+ *
+ * @author daoge_cmd
+ */
+public interface ChunkLoader {
+    FixedLoc<Float> getLocation();
+    boolean isLoaderActive();
+}
