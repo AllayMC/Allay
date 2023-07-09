8c2b2901dce68fcb7f7743a262e554eb0db7f3cc
刀哥 <3523206925@qq.com>
2023-07-10 01:07:17 +0800

    feat: Add a TODO :)

--- Allay-API/src/main/java/cn/allay/api/block/Block.java
+++ Allay-API/src/main/java/cn/allay/api/block/Block.java
@@ -7,6 +7,7 @@ import cn.allay.api.block.component.impl.base.BlockBaseComponent;
  * Allay Project 2023/3/19
  *
  * @author daoge_cmd
+ * TODO: Rename to BlockBehavior and change related things
  */
 public interface Block extends
         BlockBaseComponent,
