c1c534b7b544838dfa13324add8f70445b86d50d
CoolLoong <1542536763@qq.com>
2023-07-11 15:11:09 +0800

    fix: error in testComputeRequiredBits
    Former-commit-id: dba63e04d8efe510b0f2a30dcfc699612712171d

--- Allay-Server/src/test/java/cn/allay/server/utils/UtilsTest.java
+++ Allay-Server/src/test/java/cn/allay/server/utils/UtilsTest.java
@@ -15,15 +15,15 @@ public class UtilsTest {
     @Test
     void testComputeRequiredBits() {
         assertEquals(1, Utils.computeRequiredBits(1));
-        assertEquals(1, Utils.computeRequiredBits(2));
+        assertEquals(2, Utils.computeRequiredBits(2));
         assertEquals(2, Utils.computeRequiredBits(3));
-        assertEquals(2, Utils.computeRequiredBits(4));
+        assertEquals(3, Utils.computeRequiredBits(4));
         assertEquals(3, Utils.computeRequiredBits(5));
-        assertEquals(3, Utils.computeRequiredBits(8));
+        assertEquals(4, Utils.computeRequiredBits(8));
         assertEquals(4, Utils.computeRequiredBits(9));
-        assertEquals(4, Utils.computeRequiredBits(16));
+        assertEquals(5, Utils.computeRequiredBits(16));
         assertEquals(5, Utils.computeRequiredBits(17));
-        assertEquals(5, Utils.computeRequiredBits(32));
+        assertEquals(6, Utils.computeRequiredBits(32));
         assertEquals(6, Utils.computeRequiredBits(33));
     }
 }
