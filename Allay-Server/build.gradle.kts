c33a25edbff99aa74e663dfdfbf7646b22b43597
CoolLoong <1542536763@qq.com>
2023-07-11 17:36:37 +0800

    feat: ThroughList2Array (#44)

--- Allay-Server/build.gradle.kts
+++ Allay-Server/build.gradle.kts
@@ -27,8 +27,9 @@ tasks.processResources {
 
 tasks.jmh {
     //Add the executed test case
-    includes.add("BlockStateUpdateJMHTest")
-//    includes.add("ChunkJMHTest")
+    //includes.add("BlockStateUpdateJMHTest")
+    //includes.add("ChunkJMHTest")
+    includes.add("ThroughList2Array")
     jvmArgs.add("--enable-preview")
 }
 
---
+++ Allay-Server/src/jmh/java/cn/allay/server/ThroughList2Array.java
@@ -0,0 +1,63 @@
+package cn.allay.server;
+
+import cn.allay.api.MissingImplementationException;
+import cn.allay.api.block.property.enums.TorchFacingDirection;
+import cn.allay.api.block.property.type.BlockPropertyType;
+import cn.allay.api.data.VanillaBlockPropertyTypes;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+@State(Scope.Benchmark)
+@BenchmarkMode(Mode.Throughput)
+@Warmup(iterations = 3)
+@Threads(1)
+@Fork(1)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+public class ThroughList2Array {
+    private final BlockPropertyType.BlockPropertyValue<?, ?, ?>[] data1 = new BlockPropertyType.BlockPropertyValue[3];
+    private final ArrayList<BlockPropertyType.BlockPropertyValue<?, ?, ?>> data2 = new ArrayList<>(3);
+
+
+    @Setup
+    public void setup() throws MissingImplementationException {
+        Allay.initAllayAPI();
+        data1[0] = VanillaBlockPropertyTypes.AGE_16.createValue(5);
+        data1[1] = VanillaBlockPropertyTypes.ACTIVE.createValue(false);
+        data1[2] = VanillaBlockPropertyTypes.TORCH_FACING_DIRECTION.createValue(TorchFacingDirection.WEST);
+
+        data2.add(VanillaBlockPropertyTypes.AGE_16.createValue(5));
+        data2.add(VanillaBlockPropertyTypes.ACTIVE.createValue(false));
+        data2.add(VanillaBlockPropertyTypes.TORCH_FACING_DIRECTION.createValue(TorchFacingDirection.WEST));
+    }
+
+    @Benchmark
+    public void arrayForEach(Blackhole blackhole) {
+        for (var d : data1) {
+            blackhole.consume(d);
+        }
+    }
+
+    @Benchmark
+    public void listForEach(Blackhole blackhole) {
+        for (var d : data2) {
+            blackhole.consume(d);
+        }
+    }
+
+    @Benchmark
+    public void arrayForI(Blackhole blackhole) {
+        for (int i = 0; i < data1.length; i++) {
+            blackhole.consume(data1[i]);
+        }
+    }
+
+    @Benchmark
+    public void listForI(Blackhole blackhole) {
+        for (int i = 0; i < data2.size(); i++) {
+            blackhole.consume(data2.get(i));
+        }
+    }
+}
