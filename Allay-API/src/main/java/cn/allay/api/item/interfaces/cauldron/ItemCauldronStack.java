35e4a962b0e361634c9f56cdb6ff0b0cc0d35141
刀哥 <3523206925@qq.com>
2023-09-29 00:58:11 +0800

    refactor: inline CommonUseItemFunctions to ItemBaseComponentImpl
    Former-commit-id: 314f22c044d205474dd8a4b983ec6110baaf1a60

--- Allay-API/src/main/java/cn/allay/api/item/interfaces/cauldron/ItemCauldronStack.java
+++ Allay-API/src/main/java/cn/allay/api/item/interfaces/cauldron/ItemCauldronStack.java
@@ -2,9 +2,7 @@ package cn.allay.api.item.interfaces.cauldron;
 
 import cn.allay.api.data.VanillaBlockId;
 import cn.allay.api.data.VanillaItemId;
-import cn.allay.api.item.CommonUseItemFunctions;
 import cn.allay.api.item.ItemStack;
-import cn.allay.api.item.component.base.ItemBaseComponentImpl;
 import cn.allay.api.item.type.ItemType;
 import cn.allay.api.item.type.ItemTypeBuilder;
 
@@ -17,6 +15,5 @@ public interface ItemCauldronStack extends ItemStack {
             .builder(ItemCauldronStack.class)
             .vanillaItem(VanillaItemId.CAULDRON)
             .blockIdentifier(VanillaBlockId.CAULDRON.getIdentifier())
-            .addComponent(initInfo -> new ItemBaseComponentImpl<>(initInfo, CommonUseItemFunctions.createPlaceBlockUseOn()), ItemBaseComponentImpl.class)
             .build();
 }
