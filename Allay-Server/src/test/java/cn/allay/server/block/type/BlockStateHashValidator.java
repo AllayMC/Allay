10878fae6a1f429ddce273571672c16e4785c59d
刀哥 <3523206925@qq.com>
2023-09-30 18:33:52 +0800

    test: add BlockStateHashValidator

---
+++ Allay-Server/src/test/java/cn/allay/server/block/type/BlockStateHashValidator.java
@@ -0,0 +1,58 @@
+package cn.allay.server.block.type;
+
+import cn.allay.api.block.palette.BlockStateHashPalette;
+import cn.allay.server.block.attribute.AllayVanillaBlockAttributeRegistry;
+import cn.allay.testutils.AllayTestExtension;
+import org.cloudburstmc.nbt.NbtMap;
+import org.cloudburstmc.nbt.NbtType;
+import org.cloudburstmc.nbt.NbtUtils;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * Allay Project 2023/9/30
+ *
+ * @author daoge_cmd
+ */
+@ExtendWith(AllayTestExtension.class)
+public class BlockStateHashValidator {
+
+    static Map<Integer, NbtMap> BLOCK_ATTRIBUTES = new HashMap<>();
+
+    static {
+        try {
+            var list = ((NbtMap) NbtUtils.createGZIPReader(
+                    new BufferedInputStream(
+                            Objects.requireNonNull(
+                                    AllayVanillaBlockAttributeRegistry.class
+                                            .getClassLoader()
+                                            .getResourceAsStream("block_attributes.nbt"),
+                                    "block_attributes.nbt is missing!")
+                    )
+            ).readTag()).getList("block", NbtType.COMPOUND);
+            for (var blockAttr : list) {
+                BLOCK_ATTRIBUTES.put(blockAttr.getInt("blockStateHash"), blockAttr);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void testAllHashes() {
+        for (var expectedHash : BLOCK_ATTRIBUTES.keySet()) {
+            assertNotNull(BlockStateHashPalette.getRegistry().get(expectedHash));
+        }
+        for (var computedHash : BlockStateHashPalette.getRegistry().getContent().keySet()) {
+            assertNotNull(BLOCK_ATTRIBUTES.get(computedHash));
+        }
+    }
+}
