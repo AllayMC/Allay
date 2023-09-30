39c1ee1fe0ffe34957f09023a568fe47a57ed09d
CoolLoong <1542536763@qq.com>
2023-09-30 18:01:58 +0800

    feat: add RuntimeBlockStateDumper
    Former-commit-id: 2ad48b58499f0efebcf5b6cae9a2b15fac5da60a

---
+++ Allay-Data/src/main/java/cn/allay/data/DataUnpackUtil.java
@@ -0,0 +1,7 @@
+package cn.allay.data;
+
+public class DataUnpackUtil {
+    public static void main(String[] args) {
+        DataUnpackUtil.class.getResourceAsStream("unpacked/block_palette.nbt");
+    }
+}
