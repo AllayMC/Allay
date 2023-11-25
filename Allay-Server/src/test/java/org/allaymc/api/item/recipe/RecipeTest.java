87e2a87f7a1d1b81fa9d4e5b59c87ca457a0b7c7
刀哥 <3523206925@qq.com>
2023-11-25 18:39:08 +0800

    test: add more test for shaped recipe

--- Allay-Server/src/test/java/org/allaymc/api/item/recipe/RecipeTest.java
+++ Allay-Server/src/test/java/org/allaymc/api/item/recipe/RecipeTest.java
@@ -2,6 +2,7 @@ package org.allaymc.api.item.recipe;
 
 import org.allaymc.api.data.VanillaItemId;
 import org.allaymc.api.identifier.Identifier;
+import org.allaymc.api.item.descriptor.ItemDescriptor;
 import org.allaymc.api.item.descriptor.ItemIdentifierDescriptor;
 import org.allaymc.api.item.init.SimpleItemStackInitInfo;
 import org.allaymc.api.item.interfaces.ItemAirStack;
@@ -13,8 +14,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Allay Project 2023/11/25
@@ -23,6 +23,9 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  */
 @ExtendWith(AllayTestExtension.class)
 class RecipeTest {
+
+    public static final Map<Character, ItemDescriptor> GRASS_KEY = Map.of('x', new ItemIdentifierDescriptor(VanillaItemId.GRASS.getIdentifier()));
+
     @Test
     void testShapedRecipe() {
         ItemDiamondStack diamond = ItemDiamondStack.DIAMOND_TYPE.createItemStack(
@@ -30,14 +33,22 @@ class RecipeTest {
         );
         var grassMagic1 = ShapedRecipe
                 .builder()
-                .identifier(new Identifier("minecraft:test_shaped"))
+                .identifier(new Identifier("minecraft:grass_magic_1"))
+                .group("test_group")
+                .tags(new String[]{"test_tag"})
                 .pattern(
                         "xx",
                         "xx"
                 )
-                .keys(Map.of('x', new ItemIdentifierDescriptor(VanillaItemId.GRASS.getIdentifier())))
+                .keys(GRASS_KEY)
                 .output(diamond)
                 .build();
+
+        assertEquals(new Identifier("minecraft:grass_magic_1"), grassMagic1.getIdentifier());
+        assertEquals(ItemDiamondStack.DIAMOND_TYPE, grassMagic1.getOutput().getItemType());
+        assertEquals("test_group", grassMagic1.getGroup());
+        assertArrayEquals(new String[]{"test_tag"}, grassMagic1.getTags());
+
         var input1 = new ShapedInput(
                 grass(), grass(), air(),
                 grass(), grass(), air(),
@@ -57,25 +68,88 @@ class RecipeTest {
                 air(), grass(), grass(),
                 air(), grass(), grass()
         );
+        var input5 = new ShapedInput(
+                air(), air(), air(),
+                grass(), grass(), air(),
+                grass(), grass(), air()
+        );
 
         assertTrue(grassMagic1.match(input1));
         assertTrue(grassMagic1.match(input2));
         assertTrue(grassMagic1.match(input3));
         assertTrue(grassMagic1.match(input4));
+        assertTrue(grassMagic1.match(input5));
 
-        var input5 = new ShapedInput(
+        var input6 = new ShapedInput(
                 grass(), grass(), grass(),
                 grass(), grass(), grass(),
                 grass(), grass(), grass()
         );
-        var input6 = new ShapedInput(
+        var input7 = new ShapedInput(
                 air(), air(), air(),
                 air(), air(), air(),
                 air(), air(), air()
         );
 
-        assertFalse(grassMagic1.match(input5));
         assertFalse(grassMagic1.match(input6));
+        assertFalse(grassMagic1.match(input7));
+
+        var grassMagic2 = ShapedRecipe
+                .builder()
+                .identifier(new Identifier("minecraft:grass_magic_2"))
+                .pattern(
+                        "x",
+                        "x"
+                )
+                .keys(GRASS_KEY)
+                .output(diamond)
+                .build();
+
+        var input8 = new ShapedInput(
+                grass(), air(), air(),
+                grass(), air(), air(),
+                air(),   air(), air()
+        );
+        var input9 = new ShapedInput(
+                air(), grass(), air(),
+                air(), grass(), air(),
+                air(),   air(), air()
+        );
+        var input10 = new ShapedInput(
+                air(), air(), grass(),
+                air(), air(), grass(),
+                air(), air(), air()
+        );
+        var input11 = new ShapedInput(
+                air(), air(), air(),
+                grass(), air(), air(),
+                grass(), air(), air()
+        );
+        var input12 = new ShapedInput(
+                air(), air(), air(),
+                air(), grass(), air(),
+                air(), grass(), air()
+        );
+        var input13 = new ShapedInput(
+                air(), air(), air(),
+                air(), air(), grass(),
+                air(), air(), grass()
+        );
+
+        assertTrue(grassMagic2.match(input8));
+        assertTrue(grassMagic2.match(input9));
+        assertTrue(grassMagic2.match(input10));
+        assertTrue(grassMagic2.match(input11));
+        assertTrue(grassMagic2.match(input12));
+        assertTrue(grassMagic2.match(input13));
+
+        assertFalse(grassMagic2.match(input1));
+        assertFalse(grassMagic2.match(input2));
+        assertFalse(grassMagic2.match(input3));
+        assertFalse(grassMagic2.match(input4));
+        assertFalse(grassMagic2.match(input5));
+        assertFalse(grassMagic2.match(input6));
+        assertFalse(grassMagic2.match(input7));
     }
 
     private ItemGrassStack grass() {
