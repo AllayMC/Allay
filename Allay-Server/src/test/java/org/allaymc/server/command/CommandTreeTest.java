d18c989f34be26e5bee07f4b8e4b206f0700fe11
daoge_cmd <3523206925@qq.com>
2023-12-29 22:42:18 +0800

    feat: more test

--- Allay-Server/src/test/java/org/allaymc/server/command/CommandTreeTest.java
+++ Allay-Server/src/test/java/org/allaymc/server/command/CommandTreeTest.java
@@ -18,21 +18,40 @@ public class CommandTreeTest {
         var tree = AllayCommandTree.create();
         tree.getRoot()
                 .key("test_bool")
-                    .bool("bool")
-                    .exec(helper -> {
-                        boolean bool = helper.getResult(1);
-                        return bool ? CommandResult.SUCCESS : CommandResult.FAILED;
-                    })
-            .root()
+                .bool("bool")
+                .exec(context -> {
+                    boolean bool = context.getResult(1);
+                    return bool ? CommandResult.SUCCESS : CommandResult.FAILED;
+                })
+                .root()
                 .key("test_str")
-                    .str("str")
-                    .exec(helper -> {
-                        System.out.println((String) helper.getResult(1));
-                        return CommandResult.SUCCESS;
-                    });
+                .str("str")
+                .exec(context -> {
+                    System.out.println((String) context.getResult(1));
+                    return CommandResult.SUCCESS;
+                })
+                .root()
+                .key("test_enums")
+                .enums("enums", "a", "b", "c", "d")
+                .exec(context -> {
+                    System.out.println((String) context.getResult(1));
+                    return CommandResult.SUCCESS;
+                })
+                .root()
+                .key("test_int")
+                .intNum("int")
+                .exec(context -> {
+                    int i = context.getResult(1);
+                    return i == 1 ? CommandResult.SUCCESS : CommandResult.FAILED;
+                });
+
         var res = tree.parse(null, new String[]{"test_bool", "true"});
         assertEquals(CommandResult.SUCCESS, res);
         res = tree.parse(null, new String[]{"test_str", "a"});
+        assertEquals(CommandResult.SUCCESS, res);
+        res = tree.parse(null, new String[]{"test_enums", "b"});
+        assertEquals(CommandResult.SUCCESS, res);
+        res = tree.parse(null, new String[]{"test_int", "1"});
         assertEquals(CommandResult.SUCCESS, res);
     }
 }
