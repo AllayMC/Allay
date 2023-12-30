2e8de07e9eb7726ff9b397076c9bf122e88dda2b
daoge_cmd <3523206925@qq.com>
2023-12-30 21:04:04 +0800

    fix: fix default perm mistake

--- Allay-API/src/main/java/org/allaymc/api/perm/DefaultPermissions.java
+++ Allay-API/src/main/java/org/allaymc/api/perm/DefaultPermissions.java
@@ -10,10 +10,7 @@ import org.allaymc.api.perm.tree.PermTree;
  */
 public interface DefaultPermissions {
     PermTree VISITOR =
-            PermTree.create()
-                    .addPerm(SimpleCommand.COMMAND_PERM_PREFIX + "help")
-                    .addPerm(SimpleCommand.COMMAND_PERM_PREFIX + "tell")
-                    .addPerm(SimpleCommand.COMMAND_PERM_PREFIX + "me");
+            PermTree.create();
 
     PermTree MEMBER =
             PermTree.create()
