38e364ec9910b89a020429af17bd75d160eb61cf
daoge_cmd <3523206925@qq.com>
2024-01-20 00:08:16 +0800

    fix: fix optional node bug

--- Allay-Server/src/main/java/org/allaymc/server/command/defaults/GameRuleCommand.java
+++ Allay-Server/src/main/java/org/allaymc/server/command/defaults/GameRuleCommand.java
@@ -1,13 +1,9 @@
 package org.allaymc.server.command.defaults;
 
-import org.allaymc.api.command.CommandResult;
 import org.allaymc.api.command.SimpleCommand;
-import org.allaymc.api.command.tree.CommandContext;
 import org.allaymc.api.command.tree.CommandTree;
 import org.allaymc.api.i18n.TrKeys;
 import org.allaymc.api.world.gamerule.GameRule;
-
-import java.util.function.Function;
 
 import static org.allaymc.api.command.data.CommonEnum.GAMERULE_ENUM;
 
@@ -27,28 +23,23 @@ public class GameRuleCommand extends SimpleCommand {
     public void prepareCommandTree(CommandTree tree) {
         tree.getRoot()
                 .enums("gamerule", GAMERULE_ENUM)
-                .bool("value")
+                .str("value")
                 .optional()
-                .exec(exec())
-                .up()
-                .intNum("value")
-                .optional()
-                .exec(exec());
-    }
-
-    protected Function<CommandContext, CommandResult> exec() {
-        return context -> {
-            var world = context.getSender().getCmdExecuteLocation().dimension().getWorld();
-            var gamerule = GameRule.fromName(context.getFirstResult());
-            if (context.getSecondResult() != null) {
-                boolean value = context.getSecondResult();
-                world.setGameRule(gamerule, value);
-                context.addOutput(TrKeys.M_COMMANDS_GAMERULE_SUCCESS, gamerule.getName(), value);
-            } else {
-                boolean value = world.getWorldData().getGameRule(gamerule);
-                context.addOutput(String.valueOf(value));
-            }
-            return context.success();
-        };
+                .exec(context -> {
+                    var world = context.getSender().getCmdExecuteLocation().dimension().getWorld();
+                    var gamerule = GameRule.fromName(context.getFirstResult());
+                    if (context.getSecondResult() != null) {
+                        Object value = switch (gamerule.getType()) {
+                            case INT -> Integer.parseInt(context.getSecondResult());
+                            case BOOLEAN -> Boolean.parseBoolean(context.getSecondResult());
+                        };
+                        world.setGameRule(gamerule, value);
+                        context.addOutput(TrKeys.M_COMMANDS_GAMERULE_SUCCESS, gamerule.getName(), value);
+                    } else {
+                        Object value = world.getWorldData().getGameRule(gamerule);
+                        context.addOutput(String.valueOf(value));
+                    }
+                    return context.success();
+                });
     }
 }
