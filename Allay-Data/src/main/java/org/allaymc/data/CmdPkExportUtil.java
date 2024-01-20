8effab5a710526b127a3af50074d0a30d3b3372f
daoge_cmd <3523206925@qq.com>
2024-01-20 15:16:28 +0800

    feat: dump cmd info from bds available_commands_packet.bin

---
+++ Allay-Data/src/main/java/org/allaymc/data/CmdPkExportUtil.java
@@ -0,0 +1,42 @@
+package org.allaymc.data;
+
+import com.google.common.base.Preconditions;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import lombok.SneakyThrows;
+import org.allaymc.api.network.ProtocolInfo;
+import org.allaymc.api.utils.JSONUtils;
+import org.cloudburstmc.protocol.bedrock.codec.BedrockCodec;
+import org.cloudburstmc.protocol.bedrock.codec.BedrockCodecHelper;
+import org.cloudburstmc.protocol.bedrock.packet.AvailableCommandsPacket;
+import org.cloudburstmc.protocol.bedrock.packet.CraftingDataPacket;
+
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ * Allay Project 2024/1/20
+ *
+ * @author daoge_cmd
+ */
+public class CmdPkExportUtil {
+    private static final BedrockCodec CODEC = ProtocolInfo.PACKET_CODEC;
+
+    @SneakyThrows
+    public static void main(String[] args) {
+        BedrockCodecHelper helper = CODEC.createHelper();
+        try (InputStream resourceAsStream = RecipeExportUtil.class.getClassLoader().getResourceAsStream("available_commands_packet.bin")) {
+            Preconditions.checkNotNull(resourceAsStream);
+            ByteBuf byteBuf = Unpooled.wrappedBuffer(resourceAsStream.readAllBytes());
+            // 跳过AvailableCommandsPacket的packet id
+            byteBuf.skipBytes(1);
+            var pk = new AvailableCommandsPacket();
+            CODEC.getPacketDefinition(AvailableCommandsPacket.class).getSerializer().deserialize(byteBuf, helper, pk);
+            Files.deleteIfExists(Path.of("Allay-Data/resources/unpacked/cmd_pk.json"));
+            JSONUtils.toFile("./Allay-Data/resources/unpacked/cmd_pk.json", pk, writer -> writer.setIndent("  "));
+        }
+    }
+}
