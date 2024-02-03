7f46730686da01a65dfcfb1a0bf4f7be91f75124
daoge_cmd <3523206925@qq.com>
2024-02-03 21:52:19 +0800

    feat: flat world generator preset

--- Allay-Server/src/main/java/org/allaymc/server/world/generator/flat/FlatWorldGenerator.java
+++ Allay-Server/src/main/java/org/allaymc/server/world/generator/flat/FlatWorldGenerator.java
@@ -3,9 +3,15 @@ package org.allaymc.server.world.generator.flat;
 import org.allaymc.api.block.interfaces.BlockBedrockBehavior;
 import org.allaymc.api.block.interfaces.BlockDirtBehavior;
 import org.allaymc.api.block.interfaces.BlockGrassBehavior;
+import org.allaymc.api.block.registry.BlockTypeRegistry;
+import org.allaymc.api.block.type.BlockState;
+import org.allaymc.api.identifier.Identifier;
+import org.allaymc.api.utils.AllayStringUtils;
 import org.allaymc.api.world.generator.ChunkGenerateContext;
 import org.allaymc.api.world.generator.WorldGenerator;
 import org.allaymc.api.world.generator.WorldGeneratorType;
+
+import java.util.ArrayList;
 
 /**
  * Allay Project 2023/7/8
@@ -14,24 +20,47 @@ import org.allaymc.api.world.generator.WorldGeneratorType;
  */
 public class FlatWorldGenerator extends WorldGenerator {
 
+    protected BlockState[] layers;
+
     public FlatWorldGenerator(String preset) {
         super(preset);
+        parsePreset();
+    }
+
+    protected void parsePreset() {
+        var list = new ArrayList<BlockState>();
+        if (preset.isBlank()) {
+            var bedrock = BlockBedrockBehavior.BEDROCK_TYPE.getDefaultState();
+            var grass = BlockGrassBehavior.GRASS_TYPE.getDefaultState();
+            var dirt = BlockDirtBehavior.DIRT_TYPE.getDefaultState();
+            list.add(bedrock);
+            list.add(dirt);
+            list.add(dirt);
+            list.add(dirt);
+            list.add(grass);
+            return;
+        } else {
+            for (var layer : AllayStringUtils.fastSplit(preset, ";")) {
+                var entry = AllayStringUtils.fastTwoPartSplit(layer, "x", "");
+                var blockId = entry[0];
+                var count = Integer.parseInt(entry[1]);
+                var blockState = BlockTypeRegistry.getRegistry().get(new Identifier(blockId)).getDefaultState();
+                for (int i = 1; i <= count; i++) {
+                    list.add(blockState);
+                }
+            }
+        }
+        layers = list.toArray(BlockState[]::new);
     }
 
     @Override
     public void generate(ChunkGenerateContext context) {
-        var bedrock = BlockBedrockBehavior.BEDROCK_TYPE.getDefaultState();
-        var grass = BlockGrassBehavior.GRASS_TYPE.getDefaultState();
-        var dirt = BlockDirtBehavior.DIRT_TYPE.getDefaultState();
         var flatChunk = context.chunk();
         for (int x = 0; x < 16; x++) {
             for (int z = 0; z < 16; z++) {
-                flatChunk.setHeight(x, z, 5);
-                for (int y = 0; y < 5; y++) {
-                    if (y == 0) {
-                        flatChunk.setBlockState(x, y, z, bedrock);
-                    } else if (y == 4) flatChunk.setBlockState(x, y, z, grass);
-                    else flatChunk.setBlockState(x, y, z, dirt);
+                flatChunk.setHeight(x, z, layers.length + 1);
+                for (int y = 0; y < layers.length; y++) {
+                    flatChunk.setBlockState(x, y, z, layers[y]);
                 }
             }
         }
