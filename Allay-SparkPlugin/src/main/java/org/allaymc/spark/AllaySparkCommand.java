43a7190525f02ca8e098e8e19907df56d95f59a8
daoge_cmd <3523206925@qq.com>
2024-02-11 04:36:15 +0800

    fix: fix spark command perm

--- Allay-SparkPlugin/src/main/java/org/allaymc/spark/AllaySparkCommand.java
+++ Allay-SparkPlugin/src/main/java/org/allaymc/spark/AllaySparkCommand.java
@@ -2,6 +2,7 @@ package org.allaymc.spark;
 
 import me.lucko.spark.common.SparkPlatform;
 import org.allaymc.api.command.BaseCommand;
+import org.allaymc.api.command.Command;
 import org.allaymc.api.command.CommandResult;
 import org.allaymc.api.command.CommandSender;
 
@@ -17,7 +18,7 @@ public class AllaySparkCommand extends BaseCommand {
     private final SparkPlatform platform;
 
     public AllaySparkCommand(SparkPlatform platform) {
-        super("spark", "spark", List.of("spark"));
+        super("spark", "spark", List.of(Command.COMMAND_PERM_PREFIX + "spark"));
         this.platform = platform;
     }
 
