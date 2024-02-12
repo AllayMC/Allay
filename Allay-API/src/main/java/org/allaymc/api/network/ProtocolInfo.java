17c106829637284691268d7c7afe503196c02034
daoge_cmd <3523206925@qq.com>
2024-02-13 03:50:41 +0800

    feat: update ProtocolInfo for 1.20.60

--- Allay-API/src/main/java/org/allaymc/api/network/ProtocolInfo.java
+++ Allay-API/src/main/java/org/allaymc/api/network/ProtocolInfo.java
@@ -3,12 +3,13 @@ package org.allaymc.api.network;
 import org.allaymc.api.datastruct.SemVersion;
 import org.cloudburstmc.protocol.bedrock.codec.BedrockCodec;
 import org.cloudburstmc.protocol.bedrock.codec.v630.Bedrock_v630;
+import org.cloudburstmc.protocol.bedrock.codec.v649.Bedrock_v649;
 
 public final class ProtocolInfo {
 
-    public static final BedrockCodec PACKET_CODEC = Bedrock_v630.CODEC;
+    public static final BedrockCodec PACKET_CODEC = Bedrock_v649.CODEC;
 
-    public static final SemVersion MINECRAFT_VERSION = new SemVersion(1, 20, 50, 1, 0);
+    public static final SemVersion MINECRAFT_VERSION = new SemVersion(1, 20, 60, 1, 0);
 
     public static final int BLOCK_STATE_VERSION_NO_REVISION = (MINECRAFT_VERSION.major() << 24) | //major
                                                                (MINECRAFT_VERSION.minor() << 16) | //minor
@@ -16,6 +17,5 @@ public final class ProtocolInfo {
 
     public static final int BLOCK_STATE_VERSION = BLOCK_STATE_VERSION_NO_REVISION | MINECRAFT_VERSION.revision(); //revision
 
-    private ProtocolInfo() {
-    }
+    private ProtocolInfo() {}
 }
