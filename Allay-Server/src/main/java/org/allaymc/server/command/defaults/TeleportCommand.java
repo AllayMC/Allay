00b53eddb5369ee85c3f0997a9ee054e3a8be53f
daoge_cmd <3523206925@qq.com>
2024-02-15 19:14:20 +0800

    feat: teleport command

--- Allay-Server/src/main/java/org/allaymc/server/command/defaults/TeleportCommand.java
+++ Allay-Server/src/main/java/org/allaymc/server/command/defaults/TeleportCommand.java
@@ -2,7 +2,12 @@ package org.allaymc.server.command.defaults;
 
 import org.allaymc.api.command.SimpleCommand;
 import org.allaymc.api.command.tree.CommandTree;
+import org.allaymc.api.entity.Entity;
 import org.allaymc.api.i18n.TrKeys;
+import org.allaymc.api.math.location.Location3f;
+import org.joml.Vector3f;
+
+import java.util.List;
 
 /**
  * Allay Project 2024/2/15
@@ -17,6 +22,19 @@ public class TeleportCommand extends SimpleCommand {
 
     @Override
     public void prepareCommandTree(CommandTree tree) {
-        // TODO
+        tree.getRoot()
+                .target("targets")
+                .pos("pos")
+                .exec(context -> {
+                    List<Entity> targets = context.getFirstResult();
+                    Vector3f pos = context.getSecondResult();
+                    var loc = new Location3f(pos.x, pos.y, pos.z, context.getSender().getCmdExecuteLocation().dimension());
+                    for (Entity target : targets) {
+                        target.teleport(loc);
+                        // TODO: Entity name
+                        context.addOutput(TrKeys.M_COMMANDS_TP_SUCCESS_COORDINATES, target.getEntityType().getIdentifier(), pos.x, pos.y, pos.z);
+                    }
+                    return context.success();
+                });
     }
 }
