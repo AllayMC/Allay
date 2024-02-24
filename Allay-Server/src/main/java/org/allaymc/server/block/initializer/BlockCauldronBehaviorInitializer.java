65f5a6c1b215182f8f2c7113114396efe4c40cb3
daoge_cmd <3523206925@qq.com>
2024-02-24 19:10:59 +0800

    fix: fix BlockCauldronBehaviorInitializer

--- Allay-Server/src/main/java/org/allaymc/server/block/initializer/BlockCauldronBehaviorInitializer.java
+++ Allay-Server/src/main/java/org/allaymc/server/block/initializer/BlockCauldronBehaviorInitializer.java
@@ -5,6 +5,9 @@ import org.allaymc.api.block.type.BlockTypeBuilder;
 import org.allaymc.api.block.type.BlockTypes;
 import org.allaymc.api.data.VanillaBlockId;
 import org.allaymc.api.data.VanillaBlockPropertyTypes;
+import org.allaymc.api.math.voxelshape.CommonShapes;
+import org.allaymc.api.math.voxelshape.VoxelShape;
+import org.allaymc.server.block.component.common.BlockAttributeComponentImpl;
 
 /**
  * @author daoge_cmd <br>
@@ -16,6 +19,13 @@ public interface BlockCauldronBehaviorInitializer {
             .builder(BlockCauldronBehavior.class)
             .vanillaBlock(VanillaBlockId.CAULDRON)
             .setProperties(VanillaBlockPropertyTypes.CAULDRON_LIQUID, VanillaBlockPropertyTypes.FILL_LEVEL)
+            .addComponent(BlockAttributeComponentImpl.ofRedefinedAABB(
+                    blockState ->
+                            VoxelShape.builder()
+                                    .solid(0, 0, 0, 1, 1, 1)
+                                    .vacancy(0.125f, 0.3125f, 0.125f, 0.875f, 1, 0.875f)
+                                    .build()
+            ))
             .build();
   }
 }
