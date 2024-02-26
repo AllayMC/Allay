8f915c768ff82e6fd1759c189090d466fad84dad
daoge_cmd <3523206925@qq.com>
2024-02-26 15:07:55 +0800

    feat: let player event extend Event class

---
+++ Allay-API/src/main/java/org/allaymc/api/event/player/PlayerInitializedEvent.java
@@ -0,0 +1,18 @@
+package org.allaymc.api.event.player;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.allaymc.api.entity.interfaces.EntityPlayer;
+import org.allaymc.api.event.CancellableEvent;
+import org.allaymc.api.event.Event;
+
+/**
+ * Allay Project 2024/2/26
+ *
+ * @author daoge_cmd
+ */
+@Getter
+@AllArgsConstructor
+public class PlayerInitializedEvent extends Event implements CancellableEvent {
+    protected EntityPlayer player;
+}
