34b9e363913bd9f74db65fae80ecd51c72877d33
CoolLoong <1542536763@qq.com>
2024-06-16 13:35:30 +0800

    feat: update recipes.json

--- Allay-Data/src/main/java/org/allaymc/data/AuxToBlockStateBimapExtractor.java
+++ Allay-Data/src/main/java/org/allaymc/data/AuxToBlockStateBimapExtractor.java
@@ -20,7 +20,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-public class MarkdownListExtractor {
+public class AuxToBlockStateBimapExtractor {
     public static final BlockPropertyTypeFile BLOCK_PROPERTY_TYPE_INFO_FILE;
     public static final Path BLOCK_PROPERTY_TYPES_FILE = Path.of("Allay-Data/resources/unpacked/block_property_types.json");
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
