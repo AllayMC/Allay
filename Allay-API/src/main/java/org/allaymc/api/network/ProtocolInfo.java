83a0a6900e2fbb1f54859a79e31b5fee1185d1bb
daoge_cmd <3523206925@qq.com>
2024-06-18 00:52:01 +0800

    feat: adapt protocol changes

--- Allay-API/src/main/java/org/allaymc/api/network/ProtocolInfo.java
+++ Allay-API/src/main/java/org/allaymc/api/network/ProtocolInfo.java
@@ -4,13 +4,14 @@ import lombok.experimental.UtilityClass;
 import org.allaymc.api.datastruct.SemVersion;
 import org.cloudburstmc.protocol.bedrock.codec.BedrockCodec;
 import org.cloudburstmc.protocol.bedrock.codec.v671.Bedrock_v671;
+import org.cloudburstmc.protocol.bedrock.codec.v685.Bedrock_v685;
 
 @UtilityClass
 public final class ProtocolInfo {
 
-    public static final BedrockCodec PACKET_CODEC = Bedrock_v671.CODEC;
+    public static final BedrockCodec PACKET_CODEC = Bedrock_v685.CODEC;
 
-    public static final SemVersion MINECRAFT_VERSION = new SemVersion(1, 20, 80, 3, 0);
+    public static final SemVersion MINECRAFT_VERSION = new SemVersion(1, 21, 0, 3, 0);
 
     public static final int BLOCK_STATE_VERSION_NO_REVISION = (MINECRAFT_VERSION.major() << 24) | // major
             (MINECRAFT_VERSION.minor() << 16) | // minor
