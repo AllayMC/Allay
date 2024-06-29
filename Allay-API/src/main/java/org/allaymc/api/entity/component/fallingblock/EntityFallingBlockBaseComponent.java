f4564438534cc9ea0ed2a563499e451b6a72529b
daoge_cmd <3523206925@qq.com>
2024-06-29 18:02:34 +0800

    refactor: EntityFallingBlockBaseComponent should extends EntityBaseComponent

--- Allay-API/src/main/java/org/allaymc/api/entity/component/fallingblock/EntityFallingBlockBaseComponent.java
+++ Allay-API/src/main/java/org/allaymc/api/entity/component/fallingblock/EntityFallingBlockBaseComponent.java
@@ -1,13 +1,14 @@
 package org.allaymc.api.entity.component.fallingblock;
 
 import org.allaymc.api.block.type.BlockState;
+import org.allaymc.api.entity.component.common.EntityBaseComponent;
 
 /**
  * Allay Project 27/06/2024
  *
  * @author IWareQ
  */
-public interface EntityFallingBlockBaseComponent {
+public interface EntityFallingBlockBaseComponent extends EntityBaseComponent {
     BlockState getBlockState();
 
     void setBlockState(BlockState blockState);
