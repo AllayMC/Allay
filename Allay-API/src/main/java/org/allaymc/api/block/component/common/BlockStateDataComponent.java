14e65572f552806985654a027d96d33fd5b865f6
daoge_cmd <3523206925@qq.com>
2024-07-15 00:08:51 +0800

    docs: remove outdated docs in BlockStateDataComponent

--- Allay-API/src/main/java/org/allaymc/api/block/component/common/BlockStateDataComponent.java
+++ Allay-API/src/main/java/org/allaymc/api/block/component/common/BlockStateDataComponent.java
@@ -4,8 +4,6 @@ import org.allaymc.api.block.component.BlockComponent;
 import org.allaymc.api.block.type.BlockState;
 
 /**
- * Block Attributes Component, which are used to get the block {@link BlockStateData}
- * <p>
  * Allay Project 2023/3/25
  *
  * @author daoge_cmd | CoolLoong
