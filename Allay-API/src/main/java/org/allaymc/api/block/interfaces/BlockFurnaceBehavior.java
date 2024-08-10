9d9515dbf44972d05309184bea1cd88c3b5294d8
daoge_cmd <3523206925@qq.com>
2024-08-11 04:43:35 +0800

    fix: fix missing BlockEntityHolderComponent declaration

--- Allay-API/src/main/java/org/allaymc/api/block/interfaces/BlockFurnaceBehavior.java
+++ Allay-API/src/main/java/org/allaymc/api/block/interfaces/BlockFurnaceBehavior.java
@@ -1,10 +1,12 @@
 package org.allaymc.api.block.interfaces;
 
 import org.allaymc.api.block.BlockBehavior;
+import org.allaymc.api.block.component.common.BlockEntityHolderComponent;
+import org.allaymc.api.blockentity.interfaces.BlockEntityFurnace;
 
 /**
  * @author daoge_cmd <br>
  * Allay Project <br>
  */
-public interface BlockFurnaceBehavior extends BlockBehavior {
+public interface BlockFurnaceBehavior extends BlockBehavior, BlockEntityHolderComponent<BlockEntityFurnace> {
 }
