95d07ba0c5f023cad54336ba25af41b567c86b70
Dhaiven <hdhugo2006@outlook.com>
2024-08-12 12:38:22 +0200

    feat: add GameRuleChangeEvent

---
+++ Allay-API/src/main/java/org/allaymc/api/eventbus/event/world/GameRuleChangeEvent.java
@@ -0,0 +1,23 @@
+package org.allaymc.api.eventbus.event.world;
+
+import org.allaymc.api.eventbus.event.CancellableEvent;
+import org.allaymc.api.world.World;
+import org.allaymc.api.world.gamerule.GameRule;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+public class GameRuleChangeEvent extends WorldEvent implements CancellableEvent {
+    protected GameRule gameRule;
+    protected Object oldValue;
+    @Setter
+    protected Object newValue;
+
+    public GameRuleChangeEvent(World world, GameRule gameRule, Object oldValue, Object newValue) {
+        super(world);
+        this.gameRule = gameRule;
+        this.oldValue = oldValue;
+        this.newValue = newValue;
+    }
+}
