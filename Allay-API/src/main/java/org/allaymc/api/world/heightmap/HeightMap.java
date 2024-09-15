829379a25a17859ff87b54b85c417b412f02b58b
daoge_cmd <3523206925@qq.com>
2024-09-15 15:48:07 +0800

    docs: add javadocs for HeightMap

--- Allay-API/src/main/java/org/allaymc/api/world/heightmap/HeightMap.java
+++ Allay-API/src/main/java/org/allaymc/api/world/heightmap/HeightMap.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 /**
- * Allay Project 2023/5/30
+ * HeightMap stores the height of each pos in a chunk.
  *
  * @author Cool_Loong
  */
@@ -17,14 +17,33 @@ public final class HeightMap {
         this(new short[256]);
     }
 
+    /**
+     * Get the height of the specified position.
+     *
+     * @param x the x coordinate of the pos.
+     * @param z the z coordinate of the pos.
+     * @return the height of the pos.
+     */
     public short get(int x, int z) {
         return heights[(x << 4) | z];
     }
 
+    /**
+     * Set the height of the specified position.
+     *
+     * @param x the x coordinate of the pos.
+     * @param z the z coordinate of the pos.
+     * @param height the height of the pos.
+     */
     public void set(int x, int z, short height) {
         heights[(x << 4) | z] = height;
     }
 
+    /**
+     * Fill the height map with the specified heights.
+     *
+     * @param heights the heights to fill.
+     */
     public void fill(short[] heights) {
         System.arraycopy(heights, 0, this.heights, 0, 256);
     }
