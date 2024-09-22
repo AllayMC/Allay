f3b22ecc646e115ab6d3f94ab55334efee9c6f1b
daoge_cmd <3523206925@qq.com>
2024-09-22 21:12:33 +0800

    feat: add /gt genball cmd

--- server/src/main/java/org/allaymc/server/command/defaults/GameTestCommand.java
+++ server/src/main/java/org/allaymc/server/command/defaults/GameTestCommand.java
@@ -1,5 +1,6 @@
 package org.allaymc.server.command.defaults;
 
+import org.allaymc.api.block.type.BlockType;
 import org.allaymc.api.block.type.BlockTypes;
 import org.allaymc.api.command.SenderType;
 import org.allaymc.api.command.SimpleCommand;
@@ -18,11 +19,13 @@ import org.allaymc.api.item.data.ItemId;
 import org.allaymc.api.registry.Registries;
 import org.allaymc.api.utils.Identifier;
 import org.allaymc.api.utils.JSONUtils;
+import org.allaymc.api.utils.MathUtils;
 import org.allaymc.api.utils.TextFormat;
 import org.allaymc.server.block.type.BlockLootTable;
 import org.cloudburstmc.nbt.NbtMap;
 import org.cloudburstmc.protocol.bedrock.data.command.CommandData;
 import org.joml.Vector3f;
+import org.joml.Vector3fc;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -279,6 +282,37 @@ public class GameTestCommand extends SimpleCommand {
                 .key("printblockunder")
                 .exec((context, player) -> {
                     player.sendText(player.getDimension().getBlockState(player.getLocation().sub(0, 1, 0, new Vector3f())).toString());
+                    return context.success();
+                }, SenderType.PLAYER)
+                .root()
+                .key("genball")
+                .pos("pos")
+                .intNum("a")
+                .intNum("b")
+                .intNum("c")
+                .blockType("block")
+                .exec((context, player) -> {
+                    var pos = MathUtils.floor((Vector3fc) context.getResult(1));
+                    int a = context.getResult(2);
+                    int b = context.getResult(3);
+                    int c = context.getResult(4);
+                    int min = Math.min(a, Math.min(b, c));
+                    int max = Math.max(a, Math.max(b, c));
+                    BlockType<?> block = context.getResult(5);
+
+                    for (int x = pos.x - max; x <= pos.x + max; x++) {
+                        for (int y = pos.y - max; y <= pos.y + max; y++) {
+                            for (int z = pos.z - max; z <= pos.z + max; z++) {
+                                if (
+                                        Math.pow((x - pos.x) * ((float) min / (float) a), 2) +
+                                        Math.pow((y - pos.y) * ((float) min / (float) b), 2) +
+                                        Math.pow((z - pos.z) * ((float) min / (float) c), 2) <= min * min
+                                ) {
+                                    player.getDimension().setBlockState(x, y, z, block.getDefaultState());
+                                }
+                            }
+                        }
+                    }
                     return context.success();
                 }, SenderType.PLAYER);
     }
