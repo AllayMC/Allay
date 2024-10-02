464b9ae99961aca97e22de94096cc09479ec807e
Cdm2883 <Cdm2883@hotmail.com>
2024-10-02 18:49:01 +0800

    feat: generator with tiny java compiler

--- codegen/build.gradle.kts
+++ codegen/build.gradle.kts
@@ -1,10 +1,11 @@
 version = "1.0.0"
 
 dependencies {
-    implementation(project(":server"))
     implementation(libs.javapoet)
     implementation(libs.nbt)
     implementation(libs.gson)
+    implementation(libs.bytebuddy)
+    implementation(libs.javaparser)
     implementation(libs.jsonschema.generator)
     implementation(libs.fastutil)
     implementation(libs.annotations)
