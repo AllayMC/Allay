cc7db30ee3525e30d861169f83aef8eb1ba43b59
Cdm2883 <Cdm2883@hotmail.com>
2024-10-03 00:44:09 +0800

    feat: remove generator

--- codegen/build.gradle.kts
+++ codegen/build.gradle.kts
@@ -4,9 +4,6 @@ dependencies {
     implementation(libs.javapoet)
     implementation(libs.nbt)
     implementation(libs.gson)
-    implementation(libs.bytebuddy)
-    implementation(libs.javaparser)
-    implementation(libs.jsonschema.generator)
     implementation(libs.fastutil)
     implementation(libs.annotations)
 }
