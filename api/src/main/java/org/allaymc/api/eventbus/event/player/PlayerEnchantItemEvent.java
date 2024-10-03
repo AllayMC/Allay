37e654139179c1eba4cd818a0a58f9a7ae61f3fa
daoge_cmd <3523206925@qq.com>
2024-10-03 15:59:19 +0800

    feat: add PlayerEnchantItemEvent

---
+++ api/src/main/java/org/allaymc/api/eventbus/event/player/PlayerEnchantItemEvent.java
@@ -0,0 +1,30 @@
+package org.allaymc.api.eventbus.event.player;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.allaymc.api.entity.interfaces.EntityPlayer;
+import org.allaymc.api.eventbus.event.CancellableEvent;
+import org.allaymc.api.item.ItemStack;
+import org.allaymc.api.item.enchantment.EnchantmentInstance;
+
+import java.util.List;
+
+/**
+ * @author daoge_cmd
+ */
+@Getter
+public class PlayerEnchantItemEvent extends PlayerEvent implements CancellableEvent {
+
+    protected ItemStack itemStack;
+    @Setter
+    protected List<EnchantmentInstance> enchantments;
+    @Setter
+    protected int requiredLapisLazuliCount;
+
+    public PlayerEnchantItemEvent(EntityPlayer player, ItemStack itemStack, List<EnchantmentInstance> enchantments, int requiredLapisLazuliCount) {
+        super(player);
+        this.itemStack = itemStack;
+        this.enchantments = enchantments;
+        this.requiredLapisLazuliCount = requiredLapisLazuliCount;
+    }
+}
