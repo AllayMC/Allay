6639b701f1338c7b0be3b01dbf1c1f9ca2eacc3b
heyhey123-git <156066831+heyhey123-git@users.noreply.github.com>
2024-10-05 01:53:03 +0900

    refactor: optimize code style

--- api/src/main/java/org/allaymc/api/entity/effect/type/EffectInstantHealthType.java
+++ api/src/main/java/org/allaymc/api/entity/effect/type/EffectInstantHealthType.java
@@ -2,6 +2,7 @@ package org.allaymc.api.entity.effect.type;
 
 import org.allaymc.api.entity.Entity;
 import org.allaymc.api.entity.component.EntityDamageComponent;
+import org.allaymc.api.entity.component.EntityUndeadComponent;
 import org.allaymc.api.entity.component.attribute.EntityAttributeComponent;
 import org.allaymc.api.entity.damage.DamageContainer;
 import org.allaymc.api.entity.effect.AbstractEffectType;
@@ -20,7 +21,7 @@ public class EffectInstantHealthType extends AbstractEffectType {
     public void onAdd(Entity entity, EffectInstance effectInstance) {
         var level = effectInstance.getLevel();
 
-        if (entity.isUndead()) {
+        if (entity instanceof EntityUndeadComponent) {
             if (!(entity instanceof EntityDamageComponent damageComponent)) return;
             damageComponent.attack(DamageContainer.magicEffect((float) (3 * Math.pow(2, level))));
             return;
