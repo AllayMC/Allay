6ddb0b4a34bcb32a7bc635f44bccb5fcf664b689
heyhey123-git <156066831+heyhey123-git@users.noreply.github.com>
2024-10-10 20:19:28 +0900

    Update EntityUndeadComponent.java

--- api/src/main/java/org/allaymc/api/entity/component/EntityUndeadComponent.java
+++ api/src/main/java/org/allaymc/api/entity/component/EntityUndeadComponent.java
@@ -6,5 +6,5 @@ public interface EntityUndeadComponent extends EntityComponent {
      *
      * @return {@code true} if the entity will catch fire under sunlight, otherwise {@code false}.
      */
-    default boolean getCatchesFireUnderSunlight() {};
+    default boolean getCatchesFireUnderSunlight() { return true; };
 }
