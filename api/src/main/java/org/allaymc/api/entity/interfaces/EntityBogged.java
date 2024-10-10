8e235c7cf6e87a1dd225d554985d826e4961206a
heyhey123-git <156066831+heyhey123-git@users.noreply.github.com>
2024-10-10 20:32:11 +0900

    feat: add EntityUndeadComponent (#432)

--- api/src/main/java/org/allaymc/api/entity/interfaces/EntityBogged.java
+++ api/src/main/java/org/allaymc/api/entity/interfaces/EntityBogged.java
@@ -1,6 +1,7 @@
 package org.allaymc.api.entity.interfaces;
 
 import org.allaymc.api.entity.Entity;
+import org.allaymc.api.entity.component.EntityUndeadComponent;
 
-public interface EntityBogged extends Entity {
+public interface EntityBogged extends Entity, EntityUndeadComponent {
 }
