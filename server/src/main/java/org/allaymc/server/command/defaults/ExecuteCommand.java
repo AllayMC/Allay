21e3adf9b698a8c26ae2ad02aba6c0d76485750c
daoge_cmd <3523206925@qq.com>
2024-10-18 12:02:51 +0800

    chore: optimize import

--- server/src/main/java/org/allaymc/server/command/defaults/ExecuteCommand.java
+++ server/src/main/java/org/allaymc/server/command/defaults/ExecuteCommand.java
@@ -2,7 +2,6 @@ package org.allaymc.server.command.defaults;
 
 import org.allaymc.api.command.CommandResult;
 import org.allaymc.api.command.SimpleCommand;
-import org.allaymc.api.command.data.CommonEnums;
 import org.allaymc.api.command.tree.CommandTree;
 import org.allaymc.api.entity.Entity;
 import org.allaymc.api.i18n.TrKeys;
