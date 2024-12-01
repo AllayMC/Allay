1d03170d67be1150970dbca5d5125d368a789324
daoge_cmd <3523206925@qq.com>
2024-12-01 17:55:55 +0800

    docs: add missing javadoc for Forms.java

--- api/src/main/java/org/allaymc/api/form/Forms.java
+++ api/src/main/java/org/allaymc/api/form/Forms.java
@@ -10,14 +10,29 @@ import org.allaymc.api.form.type.SimpleForm;
  * @author daoge_cmd
  */
 public interface Forms {
+    /**
+     * Create a simple form.
+     *
+     * @return the simple form.
+     */
     static SimpleForm simple() {
         return new SimpleForm();
     }
 
+    /**
+     * Create a modal form.
+     *
+     * @return the modal form.
+     */
     static ModalForm modal() {
         return new ModalForm();
     }
 
+    /**
+     * Create a custom form.
+     *
+     * @return the custom form.
+     */
     static CustomForm custom() {
         return new CustomForm();
     }
