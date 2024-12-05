c02bfd043c03482504783c9120ac8aa8b92d4720
daoge_cmd <3523206925@qq.com>
2024-12-05 17:18:38 +0800

    feat: print dev build information in version command

--- server/src/main/java/org/allaymc/server/command/defaults/VersionCommand.java
+++ server/src/main/java/org/allaymc/server/command/defaults/VersionCommand.java
@@ -4,6 +4,7 @@ import org.allaymc.api.AllayAPI;
 import org.allaymc.api.command.SimpleCommand;
 import org.allaymc.api.command.tree.CommandTree;
 import org.allaymc.api.i18n.TrKeys;
+import org.allaymc.server.Allay;
 import org.allaymc.server.utils.GitProperties;
 
 /**
@@ -20,7 +21,12 @@ public class VersionCommand extends SimpleCommand {
     @Override
     public void prepareCommandTree(CommandTree tree) {
         tree.getRoot().exec(context -> {
-            context.addOutput(TrKeys.A_API_IMPLEMENTED, AllayAPI.getInstance().getCoreName(), GitProperties.getBranch() + "-" + GitProperties.getCommitIdAbbrev() + " " + GitProperties.getBuildVersion(), AllayAPI.API_VERSION);
+            context.addOutput(
+                    TrKeys.A_API_IMPLEMENTED,
+                    AllayAPI.getInstance().getCoreName(),
+                    GitProperties.getBranch() + "-" + GitProperties.getCommitIdAbbrev() + " " + GitProperties.getBuildVersion() + (Allay.IS_DEVELOPMENT_BUILD ? " DEV_BUILD" : ""),
+                    AllayAPI.API_VERSION
+            );
             return context.success();
         });
     }
