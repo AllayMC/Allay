073650508b39ec7c89c80404ee7a6fc652b4a84b
daoge_cmd <3523206925@qq.com>
2024-12-26 00:08:05 +0800

    feat: add "-dev" suffix to version if is dev build

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -2,19 +2,19 @@ import com.github.jengelman.gradle.plugins.shadow.transformers.Log4j2PluginsCach
 import java.io.ByteArrayOutputStream
 
 /**
- * The current version of allay-server
- *
- * This is not the same as the version of allay-api.
- * Please note that they are two things.
- */
-version = "0.1.1"
-/**
  * Indicates whether the current build is a development build.
  *
  * This value should be changed to false before release the next stable version,
  * and after releasing, change it back to true.
  */
-val isDevBuild = true;
+val isDevBuild = true
+/**
+ * The current version of allay-server
+ *
+ * This is not the same as the version of allay-api.
+ * Please note that they are two things.
+ */
+version = "0.1.1" + if (isDevBuild) "-dev" else ""
 
 plugins {
     id("jacoco")
@@ -119,7 +119,7 @@ tasks.create("cleanWorkingDir") {
 }
 
 fun getShadedJarName(): String {
-    return "allay-server-${version}-${getShortGitHash()}${if (isDevBuild) "-dev" else ""}-shaded.jar"
+    return "allay-server-${version}-${getShortGitHash()}-shaded.jar"
 }
 
 fun getShortGitHash(): String {
