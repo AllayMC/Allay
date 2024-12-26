f830156624761ce69de66291f02b90a8fb9a2488
daoge_cmd <3523206925@qq.com>
2024-12-26 23:50:28 +0800

    docs: add comments for LiquidReactionOnTouch

--- api/src/main/java/org/allaymc/api/block/component/data/LiquidReactionOnTouch.java
+++ api/src/main/java/org/allaymc/api/block/component/data/LiquidReactionOnTouch.java
@@ -4,8 +4,20 @@ package org.allaymc.api.block.component.data;
  * @author daoge_cmd
  */
 public enum LiquidReactionOnTouch {
+    /**
+     * The block will be broken without dropping anything.
+     */
     BROKEN,
+    /**
+     * The block will drop itself.
+     */
     POPPED,
+    /**
+     * The block will prevent the liquid from flowing into.
+     */
     BLOCKING,
+    /**
+     * The block won't have any reaction, also won't have any impact on the liquid just like air.
+     */
     NOREACTION
 }
