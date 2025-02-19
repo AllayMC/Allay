3173d6a0dcabadcea0b548d69cf2a85884ad5fd6
daoge_cmd <3523206925@qq.com>
2025-02-19 16:58:10 +0800

    feat: preparation works for the new actor storage feature

--- server/src/main/java/org/allaymc/server/world/storage/VanillaChunkState.java
+++ server/src/main/java/org/allaymc/server/world/storage/VanillaChunkState.java
@@ -1,10 +1,21 @@
 package org.allaymc.server.world.storage;
 
 /**
+ * Indicates the state of generation of the chunk in vanilla.
+ *
  * @author daoge_cmd
  */
 public enum VanillaChunkState {
-    NEEDS_INSTA_TICK,
+    /**
+     * Chunk needs to be ticked.
+     */
+    NEEDS_INSTANT_TICK,
+    /**
+     * Chunk needs to be populated.
+     */
     NEEDS_POPULATION,
+    /**
+     * Chunk is fully finished (which is the state generally found in world saves from vanilla).
+     */
     DONE
 }
