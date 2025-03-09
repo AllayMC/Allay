ebd1feeaff31081f976dab9db7d972e4ed84e1ef
Dmitry Luk <dimonpo96@mail.ru>
2025-03-09 16:24:03 +0400

    docs: correct

--- api/src/main/java/org/allaymc/api/block/component/BlockAnvilBaseComponent.java
+++ api/src/main/java/org/allaymc/api/block/component/BlockAnvilBaseComponent.java
@@ -6,5 +6,12 @@ import org.allaymc.api.block.type.BlockState;
  * @author IWareQ
  */
 public interface BlockAnvilBaseComponent extends BlockBaseComponent {
+    /**
+     * Applies damage to the anvil block state.
+     *
+     * @param current the current {@link BlockState}
+     *
+     * @return the updated {@link BlockState} after damage
+     */
     BlockState damage(BlockState current);
 }
