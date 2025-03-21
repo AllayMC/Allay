502c93733b964edb3a20f9b79ea0d75ddf7135af
daoge_cmd <3523206925@qq.com>
2025-03-21 21:03:55 +0800

    feat: optimize the output of `/world list`

--- server/src/main/java/org/allaymc/server/command/defaults/WorldCommand.java
+++ server/src/main/java/org/allaymc/server/command/defaults/WorldCommand.java
@@ -33,7 +33,7 @@ public class WorldCommand extends SimpleCommand {
                     for (var world : Server.getInstance().getWorldPool().getWorlds().values()) {
                         context.addOutput(
                                 "- " +
-                                TextFormat.GREEN + world.getName() + "(" + world.getWorldData().getDisplayName() + ")" +
+                                TextFormat.GREEN + world.getName() + "(" + world.getWorldData().getDisplayName() + ")" + TextFormat.RESET +
                                 TextFormat.YELLOW + "[" +
                                 world.getDimensions()
                                         .values()
