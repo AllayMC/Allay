ff2f293ad6e1b97c2de3c20dc8a5161565114516
Dmitry Luk <dimonpo96@mail.ru>
2025-03-27 14:55:19 +0400

    doc: RecipeContainer

--- api/src/main/java/org/allaymc/api/container/RecipeContainer.java
+++ api/src/main/java/org/allaymc/api/container/RecipeContainer.java
@@ -6,5 +6,10 @@ import org.allaymc.api.item.recipe.input.RecipeInput;
  * @author IWareQ
  */
 public interface RecipeContainer {
+    /**
+     * Creates a {@link RecipeInput} instance for the current container.
+     *
+     * @return a new instance of {@link RecipeInput} representing the input for the recipe
+     */
     RecipeInput createRecipeInput();
 }
