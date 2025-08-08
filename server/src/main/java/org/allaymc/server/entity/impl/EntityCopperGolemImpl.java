bf4221f8e4e7dcc2952f6ae3295c8abd5af1182b
daoge_cmd <3523206925@qq.com>
2025-08-08 23:44:52 +0800

    fix: add missing copper golem class files

---
+++ server/src/main/java/org/allaymc/server/entity/impl/EntityCopperGolemImpl.java
@@ -0,0 +1,15 @@
+package org.allaymc.server.entity.impl;
+
+import org.allaymc.api.component.interfaces.Component;
+import org.allaymc.api.entity.initinfo.EntityInitInfo;
+import org.allaymc.api.entity.interfaces.EntityCopperGolem;
+import org.allaymc.server.component.interfaces.ComponentProvider;
+
+import java.util.List;
+
+public class EntityCopperGolemImpl extends EntityImpl implements EntityCopperGolem {
+    public EntityCopperGolemImpl(EntityInitInfo initInfo,
+                                 List<ComponentProvider<? extends Component>> componentProviders) {
+        super(initInfo, componentProviders);
+    }
+}
