aa8bb525ce2d3469e6886c367a43fc34214bf26e
Zernix2077 <110056737+zernix2077@users.noreply.github.com>
2025-08-30 08:28:24 +0300

    feat: allow to specify custom respawn position for PlayerRespawnEvent

--- api/src/main/java/org/allaymc/api/eventbus/event/player/PlayerRespawnEvent.java
+++ api/src/main/java/org/allaymc/api/eventbus/event/player/PlayerRespawnEvent.java
@@ -1,11 +1,18 @@
 package org.allaymc.api.eventbus.event.player;
 
 import org.allaymc.api.entity.interfaces.EntityPlayer;
+import org.allaymc.api.math.location.Location3ic;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * @author daoge_cmd
  */
 public class PlayerRespawnEvent extends PlayerEvent {
+    @Getter
+    @Setter
+    protected Location3ic respawnLocation;
+
     public PlayerRespawnEvent(EntityPlayer player) {
         super(player);
     }
