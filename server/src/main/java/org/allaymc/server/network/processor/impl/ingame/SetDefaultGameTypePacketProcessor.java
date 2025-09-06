6d5a23c0592096ac25b3ad196e7ae411bac63846
Zernix2077 <110056737+zernix2077@users.noreply.github.com>
2025-09-06 18:02:05 +0300

    fix: in-game change of default game type will now affect current world settings instead of global configuration state

--- server/src/main/java/org/allaymc/server/network/processor/impl/ingame/SetDefaultGameTypePacketProcessor.java
+++ server/src/main/java/org/allaymc/server/network/processor/impl/ingame/SetDefaultGameTypePacketProcessor.java
@@ -1,7 +1,6 @@
 package org.allaymc.server.network.processor.impl.ingame;
 
 import org.allaymc.api.entity.interfaces.EntityPlayer;
-import org.allaymc.api.server.Server;
 import org.allaymc.server.network.processor.PacketProcessor;
 import org.cloudburstmc.protocol.bedrock.data.GameType;
 import org.cloudburstmc.protocol.bedrock.packet.BedrockPacketType;
@@ -15,7 +14,7 @@ public class SetDefaultGameTypePacketProcessor extends PacketProcessor<SetDefaul
     @Override
     public void handleSync(EntityPlayer player, SetDefaultGameTypePacket packet, long receiveTime) {
         if (!player.isOperator()) return;
-        Server.SETTINGS.genericSettings().defaultGameType(GameType.from(packet.getGamemode()));
+        player.getWorld().getWorldData().setGameType(GameType.from(packet.getGamemode()));
     }
 
     @Override
