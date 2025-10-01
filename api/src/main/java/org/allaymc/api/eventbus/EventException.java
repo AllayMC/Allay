d3cc58ccb5a84e6fa4d1c0df81a30a6a45910244
daoge_cmd <3523206925@qq.com>
2025-10-02 01:04:36 +0800

    fix: fix build

---
+++ api/src/main/java/org/allaymc/api/eventbus/EventException.java
@@ -0,0 +1,10 @@
+package org.allaymc.api.eventbus;
+
+import lombok.experimental.StandardException;
+
+/**
+ * @author daoge_cmd
+ */
+@StandardException
+public class EventException extends RuntimeException {
+}
