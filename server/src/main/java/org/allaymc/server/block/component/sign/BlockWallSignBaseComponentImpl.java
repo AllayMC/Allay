d39efb6b8b9b4ae7e4913a5b8a65613a5ccea9a1
daoge_cmd <3523206925@qq.com>
2025-10-02 14:26:29 +0800

    fix: fix wall sign direction

--- server/src/main/java/org/allaymc/server/block/component/sign/BlockWallSignBaseComponentImpl.java
+++ server/src/main/java/org/allaymc/server/block/component/sign/BlockWallSignBaseComponentImpl.java
@@ -3,12 +3,16 @@ package org.allaymc.server.block.component.sign;
 import org.allaymc.api.block.BlockBehavior;
 import org.allaymc.api.block.data.BlockFace;
 import org.allaymc.api.block.dto.Block;
+import org.allaymc.api.block.dto.PlayerInteractInfo;
 import org.allaymc.api.block.property.type.BlockPropertyTypes;
+import org.allaymc.api.block.type.BlockState;
 import org.allaymc.api.block.type.BlockType;
 import org.allaymc.api.entity.Entity;
 import org.allaymc.api.item.ItemStack;
+import org.allaymc.api.world.Dimension;
 import org.allaymc.server.block.component.BlockBaseComponentImpl;
 import org.allaymc.server.item.data.ItemId;
+import org.joml.Vector3ic;
 
 import java.util.Objects;
 import java.util.Set;
@@ -22,6 +26,15 @@ public class BlockWallSignBaseComponentImpl extends BlockBaseComponentImpl {
     public BlockWallSignBaseComponentImpl(BlockType<? extends BlockBehavior> blockType, ItemId dropItemId) {
         super(blockType);
         this.dropItemId = dropItemId;
+    }
+
+    @Override
+    public boolean place(Dimension dimension, BlockState blockState, Vector3ic placeBlockPos, PlayerInteractInfo placementInfo) {
+        return dimension.setBlockState(
+                placeBlockPos.x(), placeBlockPos.y(), placeBlockPos.z(),
+                blockState.setPropertyValue(BlockPropertyTypes.FACING_DIRECTION, placementInfo.blockFace().ordinal()),
+                placementInfo
+        );
     }
 
     @Override
