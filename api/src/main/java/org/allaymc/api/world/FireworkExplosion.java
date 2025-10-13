24cdc98fc2983dc26ed5feee6de2cfc13d82c518
daoge_cmd <3523206925@qq.com>
2025-10-13 23:55:48 +0800

    feat: fire explosion now allows multiple colors and fade colors

--- api/src/main/java/org/allaymc/api/world/FireworkExplosion.java
+++ api/src/main/java/org/allaymc/api/world/FireworkExplosion.java
@@ -4,19 +4,22 @@ import org.allaymc.api.utils.DyeColor;
 import org.allaymc.api.world.data.FireworkType;
 import org.cloudburstmc.nbt.NbtMap;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * FireworkExplosion represents an explosion of a firework.
  *
- * @param type    The type of the explosion.
- * @param color   The color of the explosion.
- * @param fade    The color of the explosion should fade into. Set this to {@code null} will disable this feature.
- * @param flicker Whether the explosion should flicker on explode.
- * @param trail   Whether the explosion should have a trail.
+ * @param type       The type of the explosion.
+ * @param colors     The colors of the explosion.
+ * @param fadeColors The colors of the explosion should fade into. Set this to an empty list will disable this feature.
+ * @param flicker    Whether the explosion should flicker on explode.
+ * @param trail      Whether the explosion should have a trail.
  * @author daoge_cmd
  */
 public record FireworkExplosion(
         FireworkType type,
-        DyeColor color, DyeColor fade,
+        List<DyeColor> colors, List<DyeColor> fadeColors,
         boolean flicker, boolean trail
 ) {
 
@@ -35,22 +38,23 @@ public record FireworkExplosion(
     public static FireworkExplosion fromNBT(NbtMap nbt) {
         var type = FireworkType.values()[nbt.getShort(TAG_FIREWORK_TYPE)];
 
-        DyeColor color = DyeColor.WHITE;
-        var colorData = nbt.getByteArray(TAG_FIREWORK_COLOR);
-        if (colorData != null && colorData.length > 0) {
-            color = DyeColor.from(colorData[0]);
+        var colors = new ArrayList<DyeColor>();
+        for (var colorIndex : nbt.getByteArray(TAG_FIREWORK_COLOR, new byte[0])) {
+            colors.add(DyeColor.from(colorIndex));
+        }
+        if (colors.isEmpty()) {
+            colors.add(DyeColor.WHITE);
         }
 
-        DyeColor fade = null;
-        var fadeData = nbt.getByteArray(TAG_FIREWORK_FADE);
-        if (fadeData != null && fadeData.length > 0) {
-            fade = DyeColor.from(nbt.getByteArray(TAG_FIREWORK_FADE)[0]);
+        var fadeColors = new ArrayList<DyeColor>();
+        for (var fadeColorIndex : nbt.getByteArray(TAG_FIREWORK_FADE, new byte[0])) {
+            fadeColors.add(DyeColor.from(fadeColorIndex));
         }
 
         var flicker = nbt.getBoolean(TAG_FIREWORK_FLICKER);
         var trail = nbt.getBoolean(TAG_FIREWORK_TRAIL);
 
-        return new FireworkExplosion(type, color, fade, flicker, trail);
+        return new FireworkExplosion(type, colors, fadeColors, flicker, trail);
     }
 
     /**
@@ -59,18 +63,20 @@ public record FireworkExplosion(
      * @return an {@code NbtMap} containing serialized firework explosion data
      */
     public NbtMap saveNBT() {
-        var builder = NbtMap.builder()
+        return NbtMap.builder()
                 .putByte(TAG_FIREWORK_TYPE, (byte) this.type.ordinal())
-                .putByteArray(TAG_FIREWORK_COLOR, new byte[]{(byte) this.color.ordinal()})
+                .putByteArray(TAG_FIREWORK_COLOR, encodeColors(this.colors))
+                .putByteArray(TAG_FIREWORK_FADE, encodeColors(this.fadeColors))
                 .putBoolean(TAG_FIREWORK_FLICKER, this.flicker)
-                .putBoolean(TAG_FIREWORK_TRAIL, this.trail);
+                .putBoolean(TAG_FIREWORK_TRAIL, this.trail)
+                .build();
+    }
 
-        if (fade != null) {
-            builder.putByteArray(TAG_FIREWORK_FADE, new byte[]{(byte) this.fade.ordinal()});
-        } else {
-            builder.putByteArray(TAG_FIREWORK_FADE, new byte[0]);
+    private static byte[] encodeColors(List<DyeColor> colors) {
+        var array = new byte[colors.size()];
+        for (int i = 0; i < colors.size(); i++) {
+            array[i] = (byte) colors.get(i).ordinal();
         }
-
-        return builder.build();
+        return array;
     }
 }
--- server/src/main/java/org/allaymc/server/item/component/ItemFireworkStarBaseComponentImpl.java
+++ server/src/main/java/org/allaymc/server/item/component/ItemFireworkStarBaseComponentImpl.java
@@ -4,8 +4,12 @@ import lombok.Getter;
 import lombok.Setter;
 import org.allaymc.api.item.ItemStackInitInfo;
 import org.allaymc.api.item.component.ItemFireworkStarBaseComponent;
+import org.allaymc.api.utils.DyeColor;
 import org.allaymc.api.world.FireworkExplosion;
 import org.cloudburstmc.nbt.NbtMap;
+
+import java.awt.*;
+import java.util.List;
 
 /**
  * @author daoge_cmd
@@ -35,9 +39,23 @@ public class ItemFireworkStarBaseComponentImpl extends ItemBaseComponentImpl imp
         if (this.explosion != null) {
             builder.putCompound(TAG_FIREWORKS_ITEM, this.explosion.saveNBT());
             // 'customColor' controls the color shown in the firework star. So we need to set it, although we never read it
-            builder.putInt(TAG_CUSTOM_COLOR, this.explosion.color().getColor().getRGB());
+            // This is actually the mix of all dye colors the firework explosion has.
+            builder.putInt(TAG_CUSTOM_COLOR, mixDyeColors(this.explosion.colors()).getRGB());
         }
 
         return builder.build();
+    }
+
+    protected static Color mixDyeColors(List<DyeColor> dyeColors) {
+        int r = 0, g = 0, b = 0;
+        for (var dyeColor : dyeColors) {
+            var color = dyeColor.getColor();
+            r += color.getRed();
+            g += color.getGreen();
+            b += color.getBlue();
+        }
+
+        int size = dyeColors.size();
+        return new Color(r / size, g / size, b / size);
     }
 }
