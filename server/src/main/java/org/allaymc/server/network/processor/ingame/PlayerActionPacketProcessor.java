2ee8a60361fa2d4031e39cb5a4452a8bc75b27cc
daoge_cmd <3523206925@qq.com>
2025-10-13 20:05:12 +0800

    fix: handle respawn in RespawnPacketProcessor

--- server/src/main/java/org/allaymc/server/network/processor/ingame/PlayerActionPacketProcessor.java
+++ server/src/main/java/org/allaymc/server/network/processor/ingame/PlayerActionPacketProcessor.java
@@ -2,9 +2,6 @@ package org.allaymc.server.network.processor.ingame;
 
 import lombok.extern.slf4j.Slf4j;
 import org.allaymc.api.entity.interfaces.EntityPlayer;
-import org.allaymc.api.eventbus.event.player.PlayerRespawnEvent;
-import org.allaymc.api.math.location.Location3d;
-import org.allaymc.api.math.location.Location3ic;
 import org.allaymc.server.network.processor.PacketProcessor;
 import org.cloudburstmc.protocol.bedrock.packet.BedrockPacketType;
 import org.cloudburstmc.protocol.bedrock.packet.PlayerActionPacket;
@@ -18,31 +15,6 @@ public class PlayerActionPacketProcessor extends PacketProcessor<PlayerActionPac
     @Override
     public PacketSignal handleAsync(EntityPlayer player, PlayerActionPacket packet, long receiveTime) {
         return switch (packet.getAction()) {
-            case RESPAWN -> {
-                if (!player.isDespawned()) {
-                    log.warn("Player {} tried to respawn but he is already spawned!", player.getOriginName());
-                    yield PacketSignal.HANDLED;
-                }
-
-                var event = new PlayerRespawnEvent(player);
-                event.setRespawnLocation(player.validateAndGetSpawnPoint());
-                event.call();
-
-                var spawnPoint = event.getRespawnLocation();
-                var spawnDimension = spawnPoint.dimension();
-                var oldDimension = player.getDimension();
-                if (oldDimension != spawnDimension) {
-                    // Respawn dimension and player's current dimension are different, need to remove player from the old dimension
-                    oldDimension.removePlayer(player, () -> {
-                        prepareForRespawn(player, spawnPoint);
-                        spawnDimension.addPlayer(player, () -> afterRespawn(player, spawnPoint));
-                    });
-                } else {
-                    prepareForRespawn(player, spawnPoint);
-                    spawnDimension.getEntityManager().addEntity(player, () -> afterRespawn(player, spawnPoint));
-                }
-                yield PacketSignal.HANDLED;
-            }
             case START_ITEM_USE_ON -> {
                 if (player.isUsingItemOnBlock()) {
                     log.debug("Player {} tried to start item use on without stopping", player.getOriginName());
@@ -63,20 +35,6 @@ public class PlayerActionPacketProcessor extends PacketProcessor<PlayerActionPac
             }
             default -> PacketSignal.UNHANDLED;
         };
-    }
-
-    private void prepareForRespawn(EntityPlayer player, Location3ic spawnPoint) {
-        player.setLocationBeforeSpawn(new Location3d(spawnPoint));
-        player.removeAllEffects();
-        player.resetHealth();
-        player.resetFoodData();
-        player.extinguish();
-        player.setAirSupplyTicks(player.getAirSupplyMaxTicks());
-    }
-
-    private void afterRespawn(EntityPlayer player, Location3ic spawnPoint) {
-        // Teleport to prevent the player from falling into strange places
-        player.teleport(spawnPoint);
     }
 
     @Override
