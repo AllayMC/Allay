fb1eb1c5c6b9029b0a04c97cdab43172a83ff867
daoge_cmd <3523206925@qq.com>
2025-10-18 03:23:44 +0800

    fix: fix a bug where players' name tags are not always shown

--- server/src/main/java/org/allaymc/server/entity/component/player/EntityPlayerBaseComponentImpl.java
+++ server/src/main/java/org/allaymc/server/entity/component/player/EntityPlayerBaseComponentImpl.java
@@ -178,6 +178,8 @@ public class EntityPlayerBaseComponentImpl extends EntityBaseComponentImpl imple
         this.verticalFlySpeed = DEFAULT_VERTICAL_FLY_SPEED;
         this.foodLevel = MAX_FOOD_LEVEL;
         this.nextSavePlayerDataTime = Integer.MAX_VALUE;
+        // Player's name tag is always shown
+        this.nameTagAlwaysShow = true;
     }
 
     @Override
