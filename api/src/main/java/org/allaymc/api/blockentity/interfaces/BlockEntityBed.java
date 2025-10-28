661c9ba15f060d79ca957012f659710656ce5c00
daoge_cmd <3523206925@qq.com>
2025-10-28 19:59:50 +0800

    fix: fix redundant interface extending in BlockEntityBed

--- api/src/main/java/org/allaymc/api/blockentity/interfaces/BlockEntityBed.java
+++ api/src/main/java/org/allaymc/api/blockentity/interfaces/BlockEntityBed.java
@@ -1,8 +1,7 @@
 package org.allaymc.api.blockentity.interfaces;
 
 import org.allaymc.api.blockentity.BlockEntity;
-import org.allaymc.api.blockentity.component.BlockEntityBaseComponent;
 import org.allaymc.api.blockentity.component.BlockEntityBedBaseComponent;
 
-public interface BlockEntityBed extends BlockEntity, BlockEntityBaseComponent, BlockEntityBedBaseComponent {
+public interface BlockEntityBed extends BlockEntity, BlockEntityBedBaseComponent {
 }
