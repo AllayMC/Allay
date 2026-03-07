72e1f01debe9bc57042066b72cac1b330b612f27
Zernix2077 <110056737+zernix2077@users.noreply.github.com>
2026-03-07 07:10:08 +0300

    feat: persist InputMode, ClientPlayMode and InputInteractionModel in Player

---
+++ api/src/main/java/org/allaymc/api/player/InputInteractionModel.java
@@ -0,0 +1,12 @@
+package org.allaymc.api.player;
+
+/**
+ * InputInteractionModel represents player's current input interaction model, as reported from the input.
+ *
+ * @author zernix2077
+ */
+public enum InputInteractionModel {
+    TOUCH,
+    CROSSHAIR,
+    CLASSIC
+}
