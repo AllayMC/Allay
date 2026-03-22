cb8b64a6b85a74756ef25d54f415054e5f0afe40
daoge_cmd <3523206925@qq.com>
2026-03-23 02:09:30 +0800

    feat: enforce maximum view distance from server settings in chunk radius handling

--- server/src/main/java/org/allaymc/server/network/processor/ingame/RequestChunkRadiusPacketProcessor.java
+++ server/src/main/java/org/allaymc/server/network/processor/ingame/RequestChunkRadiusPacketProcessor.java
@@ -1,6 +1,7 @@
 package org.allaymc.server.network.processor.ingame;
 
 import org.allaymc.api.player.Player;
+import org.allaymc.server.AllayServer;
 import org.allaymc.server.network.processor.PacketProcessor;
 import org.cloudburstmc.protocol.bedrock.packet.BedrockPacketType;
 import org.cloudburstmc.protocol.bedrock.packet.ChunkRadiusUpdatedPacket;
@@ -12,7 +13,8 @@ import org.cloudburstmc.protocol.bedrock.packet.RequestChunkRadiusPacket;
 public class RequestChunkRadiusPacketProcessor extends PacketProcessor<RequestChunkRadiusPacket> {
     @Override
     public void handleSync(Player player, RequestChunkRadiusPacket packet, long receiveTime) {
-        player.getControlledEntity().setChunkLoadingRadius(Math.min(packet.getRadius(), packet.getMaxRadius()));
+        player.getControlledEntity().setChunkLoadingRadius(
+                Math.min(Math.min(packet.getRadius(), packet.getMaxRadius()), AllayServer.getSettings().worldSettings().viewDistance()));
         var p = new ChunkRadiusUpdatedPacket();
         p.setRadius(player.getControlledEntity().getChunkLoadingRadius());
         player.sendPacket(p);
