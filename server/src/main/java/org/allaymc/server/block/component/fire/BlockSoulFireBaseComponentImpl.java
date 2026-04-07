8401e6ca3acd797cdac5046bbf48d90f6dd46996
Miroshka <lavmiron314@gmail.com>
2026-04-07 20:33:39 +0300

    Fix soul fire and fireproof dropped items (#868)

--- server/src/main/java/org/allaymc/server/block/component/fire/BlockSoulFireBaseComponentImpl.java
+++ server/src/main/java/org/allaymc/server/block/component/fire/BlockSoulFireBaseComponentImpl.java
@@ -1,12 +1,17 @@
 package org.allaymc.server.block.component.fire;
 
-import org.allaymc.server.block.component.BlockBaseComponentImpl;
 import org.allaymc.api.block.BlockBehavior;
 import org.allaymc.api.block.data.BlockFace;
 import org.allaymc.api.block.dto.Block;
+import org.allaymc.api.block.type.BlockState;
 import org.allaymc.api.block.type.BlockType;
 import org.allaymc.api.block.type.BlockTypes;
-import org.allaymc.api.block.type.BlockState;
+import org.allaymc.api.entity.Entity;
+import org.allaymc.api.entity.damage.DamageContainer;
+import org.allaymc.api.entity.damage.DamageType;
+import org.allaymc.api.entity.interfaces.EntityLiving;
+import org.allaymc.api.eventbus.event.entity.EntityCombustEvent;
+import org.allaymc.server.block.component.BlockBaseComponentImpl;
 
 /**
  * @author daoge_cmd
@@ -22,5 +27,19 @@ public class BlockSoulFireBaseComponentImpl extends BlockBaseComponentImpl {
         if (downBlockType != BlockTypes.SOUL_SAND && downBlockType != BlockTypes.SOUL_SOIL) {
             block.getDimension().setBlockState(block.getPosition(), BlockTypes.FIRE.copyPropertyValuesFrom(block.getBlockState()));
         }
+    }
+
+    @Override
+    public void onEntityInside(Block block, Entity entity) {
+        if (!(entity instanceof EntityLiving living)) {
+            return;
+        }
+
+        var combustEvent = new EntityCombustEvent(entity, EntityCombustEvent.CombusterType.BLOCK, block, 20 * 8);
+        if (combustEvent.call()) {
+            living.setOnFireTicks(combustEvent.getOnFireTicks());
+        }
+
+        living.attack(new DamageContainer(block, DamageType.FIRE, 2));
     }
 }
