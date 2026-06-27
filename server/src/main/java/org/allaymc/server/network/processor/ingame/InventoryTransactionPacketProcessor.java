e818d1a0b2c93e6917d9bac16d2b187c32ee39e3
Zernix2077 <110056737+zernix2077@users.noreply.github.com>
2026-06-27 20:42:15 +0300

    fix: inventory transaction hotbar slot desync

--- server/src/main/java/org/allaymc/server/network/processor/ingame/InventoryTransactionPacketProcessor.java
+++ server/src/main/java/org/allaymc/server/network/processor/ingame/InventoryTransactionPacketProcessor.java
@@ -58,6 +58,11 @@ public class InventoryTransactionPacketProcessor extends PacketProcessor<Invento
     @Override
     public void handleSync(Player player, InventoryTransactionPacket packet, long receiveTime) {
         var entity = player.getControlledEntity();
+
+        // Fix desync issue
+        // https://github.com/CloudburstMC/Nukkit/blob/dbbb7ca6fe7e097ba25a451f9a280a9f7b251471/src/main/java/cn/nukkit/Player.java#L4287
+        entity.setHandSlot(packet.getHotbarSlot(), false);
+
         var itemInHand = entity.getItemInHand();
         var transactionType = packet.getTransactionType();
         switch (transactionType) {
